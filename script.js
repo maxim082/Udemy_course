6c9193d530bb613b00578161d1dcb540c590c3c7
Максим <maxim.fizh@gmail.com>
2020-03-21 11:03:15 +0400

    Update for second HW

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 let money = +prompt('Ваш бюджет на месяц?');
 let time = prompt('Введите дату в формате YYYY-MM-DD');
-
 let appData = {
     money: money,
     time: new Date(time),
@@ -12,11 +11,22 @@ let appData = {
     savings: false
 }
 
-let answer1 = prompt('Введите обязательную статью расходов в этом месяце');
-let answer2 = prompt('Во сколько обойдется?');
+let count = +prompt('Сколько у вас обязательных статей расходов?')
 
-appData.expenses4[answer1] = answer2;
+for (let a = 0; a < count; a++) {
+    let answer1 = prompt('Введите обязательную статью расходов в этом месяце');
+    let answer2 = prompt('Во сколько обойдется?');
 
-alert(`Бюджет пользователя на 1 день ${appData.money/30}`);
+    if (answer1.length == 0 || answer2.length == 0) {
+        console.log('Ошибка ввода данных');
+        break;
+    } else {
+        appData.expenses[answer1] = answer2;
+    }
+}
+
+appData.moneyPerDay = appData.money/30;
+
+alert(`Бюджет пользователя на 1 день ${appData.moneyPerDay}`);
 
 console.log(appData);
