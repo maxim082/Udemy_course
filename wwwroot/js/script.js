79a24130c20a84f9032af14f8d84959dc0e343a2
Максим <maxim.fizh@gmail.com>
2020-03-31 15:23:30 +0400

    add function

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,7 +1,15 @@
 'use strict'
 
-let money = +prompt('Ваш бюджет на месяц?');
-let time = prompt('Введите дату в формате YYYY-MM-DD');
+const BUDJET_REQUEST = 'Ваш бюджет на месяц?';
+const DATE_REQUEST = 'Ваш бюджет на месяц?';
+const COUNT_COSTS_REQUEST = 'Сколько у вас обязательных статей расходов?';
+const NAME_COST_REQUEST = 'Введите обязательную статью расходов в этом месяце';
+const VALUE_COST_REQUEST = 'Во сколько обойдется?';
+const ERROR_MESSAGE = 'Ошибка ввода данных';
+
+let money = +prompt(BUDJET_REQUEST);
+let time = prompt(DATE_REQUEST);
+
 let appData = {
     money: money,
     time: new Date(time),
@@ -11,22 +19,24 @@ let appData = {
     savings: false
 }
 
-let count = +prompt('Сколько у вас обязательных статей расходов?')
+let count = +prompt(COUNT_COSTS_REQUEST)
+
+appData.moneyPerDay = calculateMoneyPerDay(appData.money);
+
+alert(`Бюджет пользователя на 1 день ${appData.moneyPerDay}`);
 
 for (let a = 0; a < count; a++) {
-    let answer1 = prompt('Введите обязательную статью расходов в этом месяце');
-    let answer2 = prompt('Во сколько обойдется?');
+    let answer1 = prompt(NAME_COST_REQUEST);
+    let answer2 = prompt(VALUE_COST_REQUEST);
 
     if (answer1.length == 0 || answer2.length == 0) {
-        console.log('Ошибка ввода данных');
+        console.log(ERROR_MESSAGE);
         break;
     } else {
         appData.expenses[answer1] = answer2;
     }
 }
 
-appData.moneyPerDay = appData.money/30;
-
-alert(`Бюджет пользователя на 1 день ${appData.moneyPerDay}`);
-
-console.log(appData);
+function calculateMoneyPerDay(money) {
+    return (money / 30).toFixed(1);
+}
