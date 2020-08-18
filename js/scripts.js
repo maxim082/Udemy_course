7371d58726fe3c40e0a1160deab2a05683c0de37
Максим <maxim.fizh@gmail.com>
2020-08-18 19:43:07 +0400

    add new script

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
-window.addEventListener('DOMContentLoader', () => {
+window.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('tabcontent'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
         tabParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
         tabsContent.forEach(item => {
             item.style.display = 'none';
         });
-        tabsContent.forEach(tab => {
-           tab.classList.remove('tabheader__item_active');
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
         });
     }
-})
+
+    function showTabsContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabsContent();
+
+    tabParent.addEventListener('click', (event) => {
+        const trg = event.target;
+
+        if (trg && trg.classList.contains('tabheader__item')) {
+            hideTabContent();
+            showTabsContent(trg.dataset.num);
+        }
+    });
+});
